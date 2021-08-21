6fefc727e05db63eff11a143343ad10e7e044279
Peter Arato <it.arato@gmail.com>
2021-08-20 21:55:52 -0400

    Fix command interpretation.

--- traf_core/src/interpreter.rs
+++ traf_core/src/interpreter.rs
@@ -1,4 +1,4 @@
-pub struct Interpreter {}
+pub struct Interpreter;
 
 impl Interpreter {
   pub fn new() -> Self {
@@ -6,20 +6,33 @@ impl Interpreter {
   }
 
   pub fn read(&self, input: Vec<u8>) -> Command {
-    let parts = input.split(|&c| c == b' ').collect::<Vec<_>>();
+    let after_cmd_space_pos = input.iter().position(|ch| ch == &b' ');
+    if after_cmd_space_pos.is_none() {
+      return Command::Invalid;
+    }
 
-    if parts[0] == b"SET" {
+    let (cmd, suffix_padded) = input.split_at(after_cmd_space_pos.unwrap());
+    let suffix = &suffix_padded[1..];
+
+    if cmd == b"SET" {
+      let after_key_space_pos = suffix.iter().position(|ch| ch == &b' ');
+      if after_key_space_pos.is_none() {
+        return Command::Invalid;
+      }
+
+      let (key, value_padded) = suffix.split_at(after_key_space_pos.unwrap());
+      let value = &value_padded[1..];
       Command::Set {
-        key: String::from_utf8(parts[1][..].into()).unwrap(),
-        value: parts[2][..].into(),
+        key: String::from_utf8(key.into()).unwrap(),
+        value: value.into(),
       }
-    } else if parts[0] == b"GET" {
+    } else if cmd == b"GET" {
       Command::Get {
-        key: String::from_utf8(parts[1][..].into()).unwrap(),
+        key: String::from_utf8(suffix.into()).unwrap(),
       }
-    } else if parts[0] == b"DELETE" {
+    } else if cmd == b"DELETE" {
       Command::Delete {
-        key: String::from_utf8(parts[1][..].into()).unwrap(),
+        key: String::from_utf8(suffix.into()).unwrap(),
       }
     } else {
       Command::Invalid
